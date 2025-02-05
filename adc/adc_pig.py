6b37d82d11fc8022e0616542d3aaeb1a11fe4a77
Tahjeira Cameron <t4camero@uwaterloo.ca>
2025-02-05 14:39:21 -0500

    dma

--- adc/adc_pig.py
+++ adc/adc_pig.py
@@ -35,11 +35,13 @@ def ConvertToDB(value, bitdepth):
 try:
     while True:
         adc_value = read_adc(0)
-        print(f"ADC Value: {adc_value}")
-        voltage = ConvertToVoltage(adc_value, 12, 3.3)  # For MCP3208 at 3.3V
-        print(f"Voltage: {voltage} v")
-        if (adc_value > 0): 
+        if (adc_value > 200): 
+            voltage = ConvertToVoltage(adc_value, 12, 3.3)  # For MCP3208 at 3.3V
             db_value = ConvertToDB(adc_value, 12)
+
+            if ()
+            print(f"Voltage: {voltage} v")
+            print(f"ADC Value: {adc_value}")
             print(f"Volume in dB: {db_value} dB")
         time.sleep(0.001)
 
