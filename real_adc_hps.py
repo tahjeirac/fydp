2c5f1700c1be320fa31cd35608bcd19ed7bcf6cc
Tahjeira Cameron <t4camero@uwaterloo.ca>
2025-02-20 03:02:42 -0500

    real adc

--- real_adc_hps.py
+++ real_adc_hps.py
@@ -125,6 +125,7 @@ def callback(indata):
         max_freq = max_ind * (SAMPLE_FREQ / WINDOW_SIZE) / NUM_HPS
 
         closest_note, closest_pitch = find_closest_note(max_freq)
+        print(closest_note)
         callback.noteBuffer.insert(0, closest_note)  # Insert into noteBuffer
         callback.noteBuffer.pop()
 
