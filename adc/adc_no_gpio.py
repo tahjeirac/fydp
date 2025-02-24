2e453b79ab403d730b018b2ff83b11a0da495255
Tahjeira Cameron <t4camero@uwaterloo.ca>
2025-02-24 00:24:24 -0500

    change channel

--- adc/adc_no_gpio.py
+++ adc/adc_no_gpio.py
@@ -8,11 +8,6 @@ spi = spidev.SpiDev()
 spi.open(0,0)
 spi.max_speed_hz=1000000
 
-#define custom chip select
-#this is done so we can use dozens of SPI devices on 1 bus
-CS_ADC = 12
-
-
 
 #same thing but for the 12-bit MCP3208
 def ReadChannel3208(channel):
@@ -27,7 +22,7 @@ def ConvertToVoltage(value, bitdepth, vref):
 delay = 0.5
  
 while True:
-  value = ReadChannel3208(1)
+  value = ReadChannel3208(0)
   voltage = ConvertToVoltage(value, 12, 3.3) #for MCP3208 at 3.3V
   #print(value)
   print(f"{voltage:.3f}")
