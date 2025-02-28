bb0e10a8aea414047b704c2f2a9783b8c2c4802f
Tahjeira Cameron <t4camero@uwaterloo.ca>
2025-02-28 15:00:50 -0500

    file

--- t/songs.py
+++ t/songs.py
@@ -29,6 +29,7 @@ class Songs:
         # print (name)
         # self.notes  = data[name]["tracks"][0]["notes"]
         # Accepts JSON data directly instead of loading from a file 
+        print(song_data)
         print(f"Setting song: {song_data['title']}")
         self.notes = song_data["notes"]  # Directly use the "notes" array
         self.NOTE_INDEX = 0  # Reset the note index
