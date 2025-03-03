c73c61de4d153eee391b7b467c8ba5a7eab1ddb4
Tahjeira Cameron <t4camero@uwaterloo.ca>
2025-03-03 03:30:48 -0500

    led numbering

--- combo.py
+++ combo.py
@@ -37,7 +37,7 @@ ALL_NOTES = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"]
 
 # NoteConversion = {'C3':7, 'B3':1, 'A3':2, 'G3': 3, 'F3':4, 'E3': 5, 'D3':6}
 # NoteConversion = {'C4':7, 'B4':1, 'A4':2, 'G4': 3, 'F4':4, 'E4': 5, 'D4':6}
-NoteConversion = {'C6':2, 'B6':3, 'A6':4, 'G5':5, 'F5':6, 'E5':7, 'D5':8, 'C5':9, 'B5': 10, 'A5':11, 'G4':12, 'F4':13, 'E4':14, 'D4':15, 'C4':16, 'B4': 17, 'A4': 18, 'G3':19, 'F3':20, 'E3':21, 'D3':22, 'C3':23}
+NoteConversion = {'C6':1, 'B5':2, 'A5':3}
 # NoteConversion = {'C6':3}
 strip = Strip()
 songs = Songs(MATCH_DELAY, strip, note_conversion=NoteConversion)
