2780c5f479d29ff7aec508e47555fb452f40a68c
Tahjeira Cameron <t4camero@uwaterloo.ca>
2025-03-04 09:20:53 -0500

    shut

--- combo.py
+++ combo.py
@@ -60,7 +60,6 @@ def fetch_song():
     while not data_recv:
       try:
           response = requests.post(f"{SERVER_URL}/receive_json")
-          strip.blinkLED(1)
           with open(file_path_no_app, 'r') as file:
             content = file.read().strip()  # Read content and remove any extra whitespace
             if content:
